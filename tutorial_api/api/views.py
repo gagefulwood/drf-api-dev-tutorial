f3dac7d65749a6372e4b2c6cc5f1afbb68bef2b9
Gage Fulwood <gagetfulwood@gmail.com>
2025-08-11 03:57:34 -0500

    Video 2: Serializers & response objects | Browsable API

--- tutorial_api/api/views.py
+++ tutorial_api/api/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True) # many is used if more than one model (a queryset)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
