84df35a7d80ebbb2f86f6877a00b06dc316ceaa8
Gage Fulwood <gagetfulwood@gmail.com>
2025-08-14 14:16:48 -0500

    Video 16: django-filter and DRF API Filtering

--- tutorial_api/api/views.py
+++ tutorial_api/api/views.py
@@ -11,10 +11,12 @@ from rest_framework.permissions import (
     AllowAny
 )
 from rest_framework.views import APIView
+from api.filters import ProductFilter
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class = ProductFilter
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
