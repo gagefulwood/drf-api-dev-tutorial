cd01b1e292a6bfe91caf5a99c23f2d21ad9e3eef
Gage Fulwood <gagetfulwood@gmail.com>
2025-08-14 19:05:42 -0500

    Video 22: Viewset Permissions | Admin v. Normal User in Django

--- tutorial_api/drf_course/settings.py
+++ tutorial_api/drf_course/settings.py
@@ -119,7 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_ATHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
